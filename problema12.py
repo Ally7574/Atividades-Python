d64bd52b029676510eec8e0f93b2c102a6dc4c01
Ally7574 <allysonvictorsouza@gmail.com>
2026-03-17 22:22:04 -0300

    ponto

---
+++ problema12.py
@@ -0,0 +1,30 @@
+num = int(input("digite seu RM: "))
+
+soma = 0
+
+un = num % 10
+soma = soma + un
+num = num // 10
+
+un = num % 10
+soma = soma + un
+num = num // 10
+
+un = num % 10
+soma = soma + un
+num = num // 10
+
+un = num % 10
+soma = soma + un
+num = num // 10
+
+un = num % 10
+soma = soma + un
+num = num // 10
+
+un = num % 10
+soma = soma + un
+num = num // 10
+
+print("soma dos numeros de seu RM: ", soma)
+
